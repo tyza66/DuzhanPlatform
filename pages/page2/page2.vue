8675f85ce5fdd124208c1983d9ad98dd698b254d
tyza66 <shun_@outlook.com>
2022-04-05 08:14:35 +0800

    Revert "Revert "opload""
    This reverts commit b9faf549c6a244a84fe3a753f51612c122cc43bc.

---
+++ pages/page2/page2.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<view class="top"></view>
+		<p class="zz">赞助我们</p>
+		<text>{{tt}}</text>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				tt:"多点点广告，求你了"
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+	.top{
+		height: var(--status-bar-height);
+		width: 100%;
+		background-color: #F8F8F8;
+	},
+	.zz{
+		font-size: 50rpx;
+		text-align: center;
+	}
+</style>
